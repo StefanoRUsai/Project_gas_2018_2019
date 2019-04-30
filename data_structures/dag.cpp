b52428f1cb98eff34d7ece9cc7ef62101f8cd0f3
Stefano R. Usai <stefano.r.usai@gmail.com>
2019-04-30 12:28:59 +0200

    setter e getter dag

--- data_structures/dag.cpp
+++ data_structures/dag.cpp
@@ -1,6 +1,41 @@
 #include "dag.h"
 
-Dag::Dag()
-{
+Dag::Dag(){}
 
+Dag::~Dag(){ }
+
+Dag::Dag(Triangle* tr){
+    this->tr = tr;
+}
+
+//getter
+Dag* Dag::getChildOne() const{
+    return this->childOne;
+}
+
+Dag* Dag::getChildTwo() const{
+    return this->childTwo;
+}
+
+Dag* Dag::getChildTrhee() const{
+    return this->childThree;
+}
+
+Triangle* Dag::getTriangle() const{
+    return this->triangle;
+}
+
+void Dag::setChildOne(Dag* node){
+    this->childOne = node;
+}
+
+void Dag::setChildTwo(Dag* node){
+    this->childTwo = node;
+}
+void Dag::setChildThree(Dag* node){
+    this->childThree = node;
+}
+
+void Dag::setTriangle(Triangle* triangle){
+    this->triangle = triangle;
 }
