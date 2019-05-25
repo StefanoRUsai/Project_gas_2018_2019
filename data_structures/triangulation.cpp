ce389335995a9fdcd960dcfa008faacb26f5e636
Stefano R. Usai <stefano.r.usai@gmail.com>
2019-05-25 13:27:27 +0200

    suddivisione diversa in quattro triangoli partendo da 2, logica rivista

--- data_structures/triangulation.cpp
+++ data_structures/triangulation.cpp
@@ -51,6 +51,7 @@ void Triangulation::subdivisionTriangle(const Point2Dd& point, Triangle* triangl
 }
 
 //suddivisione in 4 triangoli partendolo dall'edge 1
+//ricordandoci che i triangoli sono creati in senso anti orario la costruzione dovrebbe avenire nel modo seguente
 void Triangulation::subdivisionTriangleDoubleE1(const Point2Dd& point, Triangle* triangle, Node* node, Dag* dag){
 
     delaunay::Triangle* tr1_1 = Triangulation::createTriangle(triangle->v1(),  point, triangle->v3(), node, dag);
@@ -59,68 +60,84 @@ void Triangulation::subdivisionTriangleDoubleE1(const Point2Dd& point, Triangle*
 
      delaunay::Triangle* tr2_1 = nullptr;
     delaunay::Triangle* tr2_2 = nullptr;
-    //cerco il vertice opposto
-    if (triangle->v1() == triangle->e1()->v1() && triangle->v2() == triangle->e1()->v2()){
-
-        tr2_1 = Triangulation::createTriangle(triangle->e1()->v3(), point, triangle->v1(), triangle->e1()->node(), dag);
-        tr2_2 = Triangulation::createTriangle(triangle->e1()->v3(),  triangle->v2(),point,  triangle->e1()->node(), dag);
-    }
+    //cerco il vertice opposto v1==v1 e v2==v3 restituisco v2 per via del senso del triangolo
     if (triangle->v1() == triangle->e1()->v1() && triangle->v2() == triangle->e1()->v3()){
 
             tr2_1 = Triangulation::createTriangle(triangle->e1()->v2(), point, triangle->v1(), triangle->e1()->node(), dag);
             tr2_2 = Triangulation::createTriangle(triangle->e1()->v2(),  triangle->v2(),point,  triangle->e1()->node(), dag);
     }
-    if (triangle->v1() == triangle->e1()->v2() && triangle->v2() == triangle->e1()->v3()){
-            tr2_1 = Triangulation::createTriangle(triangle->e1()->v1(), point, triangle->v1(), triangle->e1()->node(), dag);
-            tr2_2 = Triangulation::createTriangle(triangle->e1()->v1(),  triangle->v2(),point,  triangle->e1()->node(), dag);
+        //cerco il vertice opposto v1==v3 e v2==v2 restituisco v2 per via del senso del triangolo
+    if (triangle->v1() == triangle->e1()->v3() && triangle->v2() == triangle->e1()->v2()){
+
+        tr2_1 = Triangulation::createTriangle(triangle->e1()->v1(), point, triangle->v1(), triangle->e1()->node(), dag);
+        tr2_2 = Triangulation::createTriangle(triangle->e1()->v1(),  triangle->v2(), point,  triangle->e1()->node(), dag);
     }
+        //cerco il vertice opposto v1==v2 e v2==v1 restituisco v3 per via del senso del triangolo
+    if (triangle->v1() == triangle->e1()->v2() && triangle->v2() == triangle->e1()->v1()){
+            tr2_1 = Triangulation::createTriangle(triangle->e1()->v3(), point, triangle->v1(), triangle->e1()->node(), dag);
+            tr2_2 = Triangulation::createTriangle(triangle->e1()->v3(),  triangle->v2(),point,  triangle->e1()->node(), dag);
+    }
+
+    this->flag=0;
 }
 //suddivisione in 4 triangoli partendolo dall'edge 2
+//ricordandoci che i triangoli sono creati in senso anti orario la costruzione dovrebbe avenire nel modo seguente
 void Triangulation::subdivisionTriangleDoubleE2(const Point2Dd& point, Triangle* triangle, Node* node, Dag* dag){
 
-    delaunay::Triangle* tr1_1 = Triangulation::createTriangle(triangle->v1(),  point, triangle->v3(), node, dag);
-    delaunay::Triangle* tr1_2 = Triangulation::createTriangle(point, triangle->v2(), triangle->v3(), node, dag);
+    delaunay::Triangle* tr1_1 = Triangulation::createTriangle(triangle->v1(), triangle->v2(), point, node, dag);
+    delaunay::Triangle* tr1_2 = Triangulation::createTriangle(triangle->v1(), point, triangle->v3(),  node, dag);
 
-     delaunay::Triangle* tr2_1 = nullptr;
+    delaunay::Triangle* tr2_1 = nullptr;
     delaunay::Triangle* tr2_2 = nullptr;
-    //cerco il vertice opposto
-    if (triangle->v1() == triangle->e2()->v1() && triangle->v2() == triangle->e2()->v2()){
+    //cerco il vertice opposto v2==v1 e v3==v3 restituisco v2 per via del senso del triangolo
+    if (triangle->v2() == triangle->e2()->v1() && triangle->v3() == triangle->e2()->v3()){
+            tr2_1 = Triangulation::createTriangle( triangle->e2()->v1(), triangle->e2()->v2(), point,  triangle->e2()->node(), dag);
+            tr2_2 = Triangulation::createTriangle( point, triangle->e2()->v2(), triangle->e2()->v3(), triangle->e2()->node(), dag);
 
-        tr2_1 = Triangulation::createTriangle(triangle->e2()->v3(), point, triangle->v1(), triangle->e2()->node(), dag);
-        tr2_2 = Triangulation::createTriangle(triangle->e2()->v3(),  triangle->v2(),point,  triangle->e2()->node(), dag);
     }
-    if (triangle->v1() == triangle->e2()->v1() && triangle->v2() == triangle->e2()->v3()){
-
-            tr2_1 = Triangulation::createTriangle(triangle->e2()->v2(), point, triangle->v1(), triangle->e2()->node(), dag);
-            tr2_2 = Triangulation::createTriangle(triangle->e2()->v2(),  triangle->v2(),point,  triangle->e2()->node(), dag);
+    //cerco il vertice opposto v2==v2 e v3==v1 restituisco v3 per via del senso del triangolo
+    if (triangle->v2() == triangle->e2()->v2() && triangle->v3() == triangle->e2()->v1()){
+        tr2_1 = Triangulation::createTriangle(triangle->e2()->v2(), triangle->e2()->v2(), point, triangle->e2()->node(), dag);
+        tr2_2 = Triangulation::createTriangle(triangle->e2()->v1(), point, triangle->e2()->v3(), triangle->e2()->node(), dag);
     }
-    if (triangle->v1() == triangle->e2()->v2() && triangle->v2() == triangle->e2()->v3()){
-            tr2_1 = Triangulation::createTriangle(triangle->e2()->v1(), point, triangle->v1(), triangle->e2()->node(), dag);
-            tr2_2 = Triangulation::createTriangle(triangle->e2()->v1(),  triangle->v2(),point,  triangle->e2()->node(), dag);
+        //cerco il vertice opposto v2==v3 e v3==v2 restituisco v1 per via del senso del triangolo
+    if (triangle->v2() == triangle->e2()->v3() && triangle->v3() == triangle->e2()->v2()){
+            tr2_1 = Triangulation::createTriangle(triangle->e2()->v3(), triangle->e2()->v1(), point, triangle->e2()->node(), dag);
+            tr2_2 = Triangulation::createTriangle(triangle->e2()->v2(), point, triangle->e2()->v1(), triangle->e2()->node(), dag);
     }
+    this->flag=0;
 }
+
 //suddivisione in 4 triangoli partendolo dall'edge 3
+//ricordandoci che i triangoli sono creati in senso anti orario la costruzione dovrebbe avenire nel modo seguente
 void Triangulation::subdivisionTriangleDoubleE3(const Point2Dd& point, Triangle* triangle, Node* node, Dag* dag){
 
-    delaunay::Triangle* tr1_1 = Triangulation::createTriangle(triangle->v1(),  point, triangle->v3(), node, dag);
+    delaunay::Triangle* tr1_1 = Triangulation::createTriangle(triangle->v1(), triangle->v2(), point, node, dag);
     delaunay::Triangle* tr1_2 = Triangulation::createTriangle(point, triangle->v2(), triangle->v3(), node, dag);
+
 
      delaunay::Triangle* tr2_1 = nullptr;
     delaunay::Triangle* tr2_2 = nullptr;
-    //cerco il vertice opposto
-    if (triangle->v1() == triangle->e3()->v1() && triangle->v2() == triangle->e3()->v2()){
-        tr2_1 = Triangulation::createTriangle(triangle->e3()->v3(), point, triangle->v1(), triangle->e3()->node(), dag);
-        tr2_2 = Triangulation::createTriangle(triangle->e3()->v3(),  triangle->v2(),point,  triangle->e3()->node(), dag);
+    //cerco il vertice opposto v1==v1 e v3==v2 restituisco v3 per via del senso del triangolo
+    if (triangle->v1() == triangle->e3()->v1() && triangle->v3() == triangle->e3()->v2()){
+        tr2_1 = Triangulation::createTriangle(triangle->e3()->v1(), point, triangle->e3()->v3(), triangle->e3()->node(), dag);
+        tr2_2 = Triangulation::createTriangle(point, triangle->e3()->v2(),  triangle->e3()->v3(),  triangle->e3()->node(), dag);
     }
-    if (triangle->v1() == triangle->e3()->v1() && triangle->v2() == triangle->e3()->v3()){
-        tr2_1 = Triangulation::createTriangle(triangle->e3()->v2(), point, triangle->v1(), triangle->e3()->node(), dag);
-        tr2_2 = Triangulation::createTriangle(triangle->e3()->v2(),  triangle->v2(),point,  triangle->e3()->node(), dag);
+    //cerco il vertice opposto v1==v2 e v3==v3 restituisco v2 per via del senso del triangolo
+    if (triangle->v1() == triangle->e3()->v2() && triangle->v3() == triangle->e3()->v3()){
+        tr2_1 = Triangulation::createTriangle(triangle->e3()->v2(), point, triangle->e3()->v1(), triangle->e3()->node(), dag);
+        tr2_2 = Triangulation::createTriangle(point,  triangle->e3()->v3(), triangle->e3()->v1(),  triangle->e3()->node(), dag);
     }
-    if (triangle->v1() == triangle->e3()->v2() && triangle->v2() == triangle->e3()->v3()){
-        tr2_1 = Triangulation::createTriangle(triangle->e3()->v1(), point, triangle->v1(), triangle->e3()->node(), dag);
-        tr2_2 = Triangulation::createTriangle(triangle->e3()->v1(),  triangle->v2(),point,  triangle->e3()->node(), dag);
+
+    //cerco il vertice opposto v1==v3 e v3==v1 restituisco v2 per via del senso del triangolo
+    if (triangle->v1() == triangle->e3()->v2() && triangle->v2() == triangle->e3()->v1()){
+            tr2_1 = Triangulation::createTriangle(triangle->e3()->v3(), point, triangle->e3()->v2(), triangle->e3()->node(), dag);
+            tr2_2 = Triangulation::createTriangle(point, triangle->e3()->v1(),  triangle->v2(),  triangle->e3()->node(), dag);
     }
+
+    this->flag=0;
 }
+
 
 
 Triangle* Triangulation::createTriangle(const Point2Dd& one,const Point2Dd& two, const Point2Dd& three, Node *node, Dag *dag){
